4a8910e1f4ba69a3f1a9d8b683ed59307e3d6a53
dkenziee <145787315+dkenziee@users.noreply.github.com>
2023-11-15 02:34:48 +0700

    Add files via upload

---
+++ fungsi dengan parameter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 02:07:15 2023
+
+@author: ASUS
+"""
+
+def get_ordinal(number):
+    if 10 <= number % 100 <= 20:
+        suffix = 'th' 
+    else: 
+        suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(number % 10, 'th') 
+    return number, suffix
+while True:
+    try:    
+        user_input = int(input("Masukkan angka (0 untuk keluar): ")) 
+        if user_input == 0: 
+            break 
+        result = get_ordinal(user_input) 
+        print(result) 
+    except ValueError: 
+        print("Masukkan harus berupa angka.")
